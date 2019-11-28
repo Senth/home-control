77dcbd2feb71c505888345730af43545a54c8121
Matteus Magnusson <senth.wallace@gmail.com>
2019-11-28 10:59:11 +0100

    Add scheduling ability.

--- homecontrol/__main__.py
+++ homecontrol/__main__.py
@@ -1,4 +1,23 @@
-from .tradfri_gateway import Lights, Groups
+from .tradfri_gateway import Lights, Groups, TradfriGateway
+from .networkdevice import NetworkDevices
+from .weather import Weather
+from apscheduler.schedulers.blocking import BlockingScheduler
 
-Lights.update_lights()
-Lights.update_lights()
+
+# Initial update
+Weather.update()
+Lights.update()
+Groups.update()
+NetworkDevices.update()
+
+
+# Schedule stuff
+scheduler = BlockingScheduler()
+
+# Update information
+scheduler.add_job(Weather.update(), 'cron', hour='*', minute=2)
+scheduler.add_job(Lights.update(), 'interval', minutes=5)
+scheduler.add_job(Groups.update(), 'interval', minutes=5)
+scheduler.add_job(NetworkDevices.update(), 'interval', seconds=15)
+
+# Schedule events/commands
