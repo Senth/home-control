268c0d67a0a9699666bb5387d766c439bef22994
Matteus Magnusson <senth.wallace@gmail.com>
2019-12-13 13:24:17 +0100

    Some devices need to be offline longer

--- homecontrol/networkdevice.py
+++ homecontrol/networkdevice.py
@@ -6,13 +6,17 @@ devices = []
 
 
 logger = logging.getLogger(__name__)
+OFF_TIMES_DEFAULT = 10
+TIMEOUT_DEFAULT = 4
 
 
 class NetworkDevice:
     """Checks if a device is on or not."""
-    def __init__(self, ip):
+    def __init__(self, ip, off_times = OFF_TIMES_DEFAULT, timeout = TIMEOUT_DEFAULT):
         self.ip = ip
-        self.off_times = 5
+        self.off_times = off_times
+        self.off_times_check = off_times
+        self.timeout = str(timeout)
         devices.append(self)
 
     def is_on(self):
@@ -21,11 +25,11 @@ class NetworkDevice:
         :return: False if the 5 or more last pings were unsuccessful (read device turned off or unavailable.
         Otherwise return True. This is used since sometimes a Wifi device will won't respond before the timeout.
         """
-        return self.off_times < 5
+        return self.off_times < self.off_times_check
 
     def ping_device(self):
         try:
-            run(['ping', '-c', '1', '-W', '3', self.ip], check=True)
+            run(['ping', '-c', '1', '-W', self.timeout, self.ip], check=True)
             self.off_times = 0
         except CalledProcessError:
             self.off_times += 1
@@ -34,10 +38,10 @@ class NetworkDevice:
 
 
 class NetworkDevices:
-    mina = NetworkDevice("192.168.0.248")
-    tv = NetworkDevice("192.168.0.2")
-    mobile_matteus = NetworkDevice("192.168.0.200")
-    mobile_emma = NetworkDevice("192.168.0.201")
+    mina = NetworkDevice("192.168.0.248", off_times=2, timeout=1)
+    tv = NetworkDevice("192.168.0.2", off_times=5, timeout=3)
+    mobile_matteus = NetworkDevice("192.168.0.200", off_times=10, timeout=4)
+    mobile_emma = NetworkDevice("192.168.0.201", off_times=30, timeout=6)
 
     @staticmethod
     def is_someone_home():
