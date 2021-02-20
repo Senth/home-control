160bf13804322b28a2a998bff3d8e913d107fd56
Matteus Magnusson <senth.wallace@gmail.com>
2021-02-20 12:24:28 +0100

    🚧 🐛 Fix null access if device or group wasn't found

--- homecontrol/smart_devices/tradfri/light.py
+++ homecontrol/smart_devices/tradfri/light.py
@@ -9,29 +9,36 @@ class TradfriLight(TradfriDevice):
         super().__init__(name)
 
     def turn_on(self) -> None:
-        Api.execute(self._device.light_control.set_state(True))
+        if self._device:
+            Api.execute(self._device.light_control.set_state(True))
 
     def turn_off(self) -> None:
-        Api.execute(self._device.light_control.set_state(False))
+        if self._device:
+            Api.execute(self._device.light_control.set_state(False))
 
     def toggle(self) -> None:
-        new_state = not self.is_on()
-        Api.execute(self._device.light_control.set_state(new_state))
+        if self._device:
+            new_state = not self.is_on()
+            Api.execute(self._device.light_control.set_state(new_state))
 
     def is_on(self) -> bool:
-        self.update()
-        return bool(self._device.light_control.lights[0].state)
+        if self._device:
+            self.update()
+            return bool(self._device.light_control.lights[0].state)
+        return False
 
     def dim(self, value: Union[float, int], transition_time: float = 1) -> None:
-        if self._device.light_control.can_set_dimmer:
-            normalized_value = Interface.normalize_dim(value)
-            Api.execute(self._device.light_control.set_dimmer(normalized_value))
+        if self._device:
+            if self._device.light_control.can_set_dimmer:
+                normalized_value = Interface.normalize_dim(value)
+                Api.execute(self._device.light_control.set_dimmer(normalized_value))
 
     def color_xy(self, x: int, y: int, transition_time: float = 1) -> None:
-        if self._device.light_control.can_set_xy:
-            normalized_time = Api.seconds_to_tradfri(transition_time)
-            Api.execute(
-                self._device.light_control.set_xy_color(
-                    x, y, transition_time=normalized_time
+        if self._device:
+            if self._device.light_control.can_set_xy:
+                normalized_time = Api.seconds_to_tradfri(transition_time)
+                Api.execute(
+                    self._device.light_control.set_xy_color(
+                        x, y, transition_time=normalized_time
+                    )
                 )
-            )
--- homecontrol/smart_devices/tradfri/socket.py
+++ homecontrol/smart_devices/tradfri/socket.py
@@ -7,15 +7,20 @@ class TradfriSocket(TradfriDevice):
         super().__init__(name)
 
     def turn_on(self) -> None:
-        Api.execute(self._device.socket_control.set_state(True))
+        if self._device:
+            Api.execute(self._device.socket_control.set_state(True))
 
     def turn_off(self) -> None:
-        Api.execute(self._device.socket_control.set_state(False))
+        if self._device:
+            Api.execute(self._device.socket_control.set_state(False))
 
     def toggle(self) -> None:
-        new_state = not self.is_on()
-        Api.execute(self._device.socket_control.set_state(new_state))
+        if self._device:
+            new_state = not self.is_on()
+            Api.execute(self._device.socket_control.set_state(new_state))
 
     def is_on(self) -> bool:
-        self.update()
-        return bool(self._device.socket_control.sockets[0].state)
+        if self._device:
+            self.update()
+            return bool(self._device.socket_control.sockets[0].state)
+        return False
