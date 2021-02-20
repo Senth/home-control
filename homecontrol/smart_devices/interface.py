0c04dab5a1b4e2029160fd40ce3d3a38de6ee0f9
Matteus Magnusson <senth.wallace@gmail.com>
2021-02-20 12:21:48 +0100

    🚧 Add Devices and Groups
    - Finished implementation of Tradfri
      - light
      - device
      - group
      - socket

--- homecontrol/smart_devices/interface.py
+++ homecontrol/smart_devices/interface.py
@@ -8,13 +8,16 @@ class Interface:
         self.name = name
 
     def turn_on(self) -> None:
-        raise NotImplementedError()
+        pass
 
     def turn_off(self) -> None:
-        raise NotImplementedError()
+        pass
 
     def toggle(self) -> None:
-        raise NotImplementedError()
+        pass
+
+    def is_on(self) -> None:
+        pass
 
     def dim(self, value: Union[float, int], transition_time: float = 1) -> None:
         """Dim lights and groups
@@ -23,10 +26,18 @@ class Interface:
             value (Union[float, int]): Dim between 0.0 and 1.0 (0.0 turns off the lights), or 0 and 254.
             transition_time (float, optional): In seconds. Defaults to 1.
         """
-        raise NotImplementedError()
+        pass
+
+    @staticmethod
+    def normalize_dim(value: Union[float, int]) -> int:
+        """Normalize dim value to be between 0 and 254"""
+        if isinstance(value, float):
+            value = round(value * 254)
+        normalized = max(0, min(254, value))
+        return normalized
 
     def update(self) -> None:
-        raise NotImplementedError()
+        pass
 
     def color_xy(self, x: int, y: int, transition_time: float = 1) -> None:
         """Set the color of the light or group
@@ -34,4 +45,4 @@ class Interface:
         Args:
             transition_time (float, optional): In seconds. Defaults to 1.
         """
-        raise NotImplementedError()
+        pass
