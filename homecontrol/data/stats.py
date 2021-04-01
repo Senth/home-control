52a73c2dc5da465fef18f7137958704145ba721f
Matteus Magnusson <senth.wallace@gmail.com>
2021-04-01 19:47:00 +0200

    Remove comma at end of file

--- homecontrol/data/stats.py
+++ homecontrol/data/stats.py
@@ -10,9 +10,9 @@ class Stats:
         if config.stats_file:
             # Calculate date
             currentDate = datetime.today().strftime("%Y-%m-%d %H:%M")
-            output = f"{currentDate},{category},{value},\n"
+            output = f"{currentDate},{category},{value}\n"
             try:
-                with open(config.stats_file, "a") as file:
+                with open(config.stats_file, "a+") as file:
                     file.write(output)
             except:
                 logger.warning("Couldn't log stat", exc_info=True)
