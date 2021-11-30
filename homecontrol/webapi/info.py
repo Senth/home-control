fce96ece5d70006713c2e8413c7bf1558826b820
Senth <senth.wallace@gmail.com>
2021-11-30 22:08:54 +0100

    ✨ Provide network information through API

--- homecontrol/webapi/info.py
+++ homecontrol/webapi/info.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, jsonify
 from flask.wrappers import Response
+from homecontrol.data.network.guest_of import GuestOf
 
+from ..data.network import Network
 from ..data.weather import Weather
 
 get_info_blueprint = Blueprint("get_info", __package__)
@@ -12,6 +14,21 @@ def get_info() -> Response:
         "weather": {
             "is_raining": int(Weather.is_raining()),
             "cloud_cover": Weather.get_cloud_coverage(),
+        },
+        "network": {
+            "guests": {
+                "any": Network.is_guest_home(),
+                "both": Network.is_guest_home(GuestOf.both),
+                "emma": Network.is_guest_home(GuestOf.emma),
+                "matteus": Network.is_guest_home(GuestOf.matteus),
+            },
+            "devices": {
+                "zen": Network.zen.is_on(),
+                "work_matteus": Network.work_matteus.is_on(),
+                "tv": Network.tv.is_on(),
+                "mobile_matteus": Network.mobile_matteus.is_on(),
+                "mobile_emma": Network.mobile_emma.is_on(),
+            },
         },
     }
     return jsonify(info)
